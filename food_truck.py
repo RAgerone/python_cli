3237000c5fea979d227b2f133fde9977d05784af
Roxanne Agerone <argonkryptonneon@gmail.com>
2018-06-27 20:07:15 -0700

    added ascii table

--- food_truck.py
+++ food_truck.py
@@ -1,36 +1,59 @@
 import click
 import requests
 from datetime import datetime
+from terminaltables import AsciiTable
 from time import gmtime, strftime
 
 
 
 
 
-url = "https://data.sfgov.org/Economy-and-Community/Mobile-Food-Schedule/jjew-r69b"
+url = "https://data.sfgov.org/resource/jjew-r69b.json"
 
 @click.command()
 @click.option('--time', default=1, prompt='The time you want to get food in format HH:MM', help='The time you would like food today')
 @click.option('--date', prompt='The date you want to get food in format MMDD',
               help='The date you want food')
+
+
+
 def get_trucks(time=None, date=None):
     """Simple program that greets NAME for a total of COUNT times."""
-    days_of_week = {
-        'Monday':'1',
-        'Tuesday':'2',
-        'Wednesday':'3',
-        'Thursday':'4',
-        'Friday':'5',
-        'Saturday':'6',
-        'Sunday':'7'
-    }
+
     # query params dayorder=5 gives friday
 
-    if not time and not date:
-        current_datetime = datetime.now()
-    payload = {'dayorder':current_datetime.weekday(),                       '$where':f"start24 < {current_datetime.strftime('%H:%m')}",
-               ""$where":f"end24 > {current_datetime.strftime('%H:%m')}", $order":"applicant"}
+    # if not time and not date:
+    def add_elements(truc, table_data):
+        table_data.append([truck["applicant"], truck['location']])
+        click.echo(truck)
+        table = AsciiTable(table_data)
+        click.echo(table)
+    current_datetime = datetime.now()
 
-    r = requests.get(url, params=payload)
+    payload = {
+        "dayorder": current_datetime.weekday(),
+        '$where': ['banana',
+            f"start24 < '{current_datetime.strftime('%H:%m')}'",
+            f"end24 > '{current_datetime.strftime('%H:%m')}'"
+    ],
+        "$order":"applicant"
+    }
+    click.echo(payload)
+    r = requests.get(url, params=None)
+    click.echo(r.url)
+    table_data = [
+        ['Name', 'Address']
+    ]
+    click.echo(url)
+    start = 0
+    ten_more = True
+    click.echo(r)
+    for truck in r.json():
+        add_elements(truck, table_data)
+        start += 1
+        if start%10 == 0:
+            table = AsciiTable(table_data)
+            click.echo(table)
+
 
     # display name and address alphabetically by name
